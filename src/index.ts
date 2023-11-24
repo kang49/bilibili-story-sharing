40c79bf3d6133f0801eedf9fdc8b9b97998361ad
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-11-24 22:03:25 +0700

    Get with all slash

--- src/index.ts
+++ src/index.ts
@@ -17,10 +17,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Origin, Authorization');
     next();
-});  
+});
 
 // API Route
-app.get('/api', async (req, res) => {
+app.get('/\/+api/', async (req, res) => {
     const { biliLink } = req.query;
 
     // Convert to string
